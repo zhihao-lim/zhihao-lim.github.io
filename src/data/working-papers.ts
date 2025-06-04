bab25062ae432f448ae2ed3906f44c51b67e2b4a
Zhi Hao Lim <lim_zhihao@hotmail.sg>
2025-06-04 14:12:04 +0800

    Add all changes to repository

--- src/data/working-papers.ts
+++ src/data/working-papers.ts
@@ -8,8 +8,7 @@ export interface WorkingPaperItem {
 
 export const workingPapersData: WorkingPaperItem[] = [
   {
-    title: "The Dynamics of Goal-Setting: Evidence from a Natural Field Experiment",
+    title: "The Dynamics of Goal-Setting: Evidence from a Natural Field Experiment (with Lorenz Goette and Hua-Jing Han)",
     description: "In a field experiment across two residential colleges, we investigate responses to evolving goal difficulty using real-time feedback and moral suasion in resource conservation. In phase 1, both moderate and hard goals lead to similar conservation effects. In phase 2, after adjusting goals to an intermediate level, treatment effects diverge due to the hard-goal group's underperformance, reducing the effect by 30%. Throughout the intervention, the moderate-goal group's high baseline users maintain stronger conservation effects than their counterparts in the hard-goal group. Our findings suggest excessively challenging goals can damage motivation, with subsequent goal adjustments failing to reverse the initial imprint.",
-    collaborators: ["Lorenz Goette", "Hua-Jing Han"]
   }
 ]; 
